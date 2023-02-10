edb343b502af97125afcebbeade15e17a8629e97
Jonas <jonasericsson90@hotmail.com>
2023-02-10 22:00:28 +0100

    Cleaned up in skeleton and created files for the consts

--- react-konva/src/App.tsx
+++ react-konva/src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
 import './App.css';
-import { CanvasRendering } from './components/CanvasRendering';
 import { Skeleton } from './components/Skeleton';
 
 function App() {
   return (
     <div className="App">
       <Skeleton />
-      {/* <CanvasRendering/> */}
     </div>
   );
 }
