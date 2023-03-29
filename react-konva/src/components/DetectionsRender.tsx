71f99d8adada837368532e31cf78a281d0b3ea57
Jonas <jonasericsson90@hotmail.com>
2023-03-29 13:54:34 +0200

    Added blur effects

--- react-konva/src/components/DetectionsRender.tsx
+++ react-konva/src/components/DetectionsRender.tsx
@@ -1,11 +1,20 @@
-import { FC } from 'react';
+import Konva from 'konva';
+import '../scss/detectionsrender.scss';
+import { FC, useEffect, useRef } from 'react';
 import { Line, Text } from 'react-konva';
 interface Props {
   detections: Array<[]>;
   groups: any;
 }
-
 export const DetectionsRender: FC<Props> = ({ detections }) => {
+  const ref = useRef<any>();
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.cache({ offset: 10 });
+    }
+  }, [ref]);
+
   function getMultipliedOfPostions(
     coordinatesValue: number,
     heightWidthValue: number
@@ -43,11 +52,35 @@ export const DetectionsRender: FC<Props> = ({ detections }) => {
                 <>
                   <Text
                     x={detectionPoints[0]}
-                    y={detectionPoints[1] - 20}
+                    y={detectionPoints[1] - 25}
                     text={detectionLabel}
                     fontSize={20}
                     fill="rgb(30,234,8)"
                   />
+                  <Line
+                    id="LineOne"
+                    points={[
+                      detectionPoints[0],
+                      detectionPoints[1],
+                      detectionPoints[2],
+                      detectionPoints[1],
+                      detectionPoints[2],
+                      detectionPoints[3],
+                      detectionPoints[0],
+                      detectionPoints[3],
+                    ]}
+                    ref={ref}
+                    filters={[
+                      Konva.Filters.Noise,
+                      Konva.Filters.Pixelate,
+                      Konva.Filters.Blur,
+                    ]}
+                    noise={5}
+                    radiusBlur={30}
+                    pixelSize={10}
+                    fill="rgba(0,0,0, 0.9)"
+                    closed
+                  ></Line>
                   <Line
                     points={[
                       detectionPoints[0],
@@ -60,7 +93,7 @@ export const DetectionsRender: FC<Props> = ({ detections }) => {
                       detectionPoints[3],
                     ]}
                     closed
-                    strokeWidth={1}
+                    strokeWidth={4}
                     stroke="rgb(30,234,8)"
                     shadowBlur={1}
                     shadowColor="rgb(30,234,8)"
